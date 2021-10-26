a1a3f66a5ba554d0c9549985865b1aad14e65f90
avinash4775 <91842984+avinash4775@users.noreply.github.com>
2021-10-27 00:46:50 +0530

    Update subtraction_operation_on_set.c

--- subtraction_operation_on_set.c
+++ subtraction_operation_on_set.c
@@ -3,21 +3,21 @@
 int main()
 {
 int a[10],b[10],num1,num2,i=0,j=0,count=0;
-printf("Enter the no of elements of the array1:");
+printf("Enter the no of elements of the Set1:");
 scanf("%d",&num1);
-printf("Enter the elements of the array:");
+printf("Enter the elements of the Set1:");
 for(i=0;i<num1;i++)
 {
 scanf("%d",&a[i]);
 }
-printf("Enter the no of elements of the array2: ");
+printf("Enter the no of elements of the Set2: ");
 scanf("%d",&num2);
-printf("Enter the elements of the array2:");
+printf("Enter the elements of the Set2:");
 for(i=0;i<num2;i++)
 {
 scanf("%d",&b[i]);
 }
-printf("Set difference array1-array2:\n");
+printf("Set difference Set1-set2:\n");
   for(i=0;i<num1;i++)
   {
   for(j=0;j<num2;j++)
