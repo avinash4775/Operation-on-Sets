fb6d1d099f0a9713d7f07edbb93d557df1f0ba3d
avinash4775 <91842984+avinash4775@users.noreply.github.com>
2021-10-27 00:44:54 +0530

    Create subtraction_operation_on_set.c

---
+++ subtraction_operation_on_set.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+int a[10],b[10],num1,num2,i=0,j=0,count=0;
+printf("Enter the no of elements of the array1:");
+scanf("%d",&num1);
+printf("Enter the elements of the array:");
+for(i=0;i<num1;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("Enter the no of elements of the array2: ");
+scanf("%d",&num2);
+printf("Enter the elements of the array2:");
+for(i=0;i<num2;i++)
+{
+scanf("%d",&b[i]);
+}
+printf("Set difference array1-array2:\n");
+  for(i=0;i<num1;i++)
+  {
+  for(j=0;j<num2;j++)
+  {if(a[i]==b[j])
+    count++;
+     }
+     if(count==0)
+     printf("%d ,",a[i]);
+     count=0;
+     }
+   
+    
+
+    return 0;
+}
